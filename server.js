74ac56649279f17a542edc295a1b65592dd43096
Philip Hoover <philhoov90@gmail.com>
2018-10-09 16:40:44 -0600

    Connected my Database, can see json in browser

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const methodOverride = require('method-override');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
+const Pokemon = require('./pokemon')
 
+app.get('/', (req, res) => {
+    res.send('Gotta catch em all...')
+  });
 
+  app.get('/pokemon', (req, res) => {
+    const pokeString = JSON.stringify(Pokemon)
+    res.send(pokeString)
+  });
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
