8acdc7995fcb1f3c6f34902114b5f97cd5dddb5c
Philip Hoover <philhoov90@gmail.com>
2018-10-09 16:15:23 -0600

    Commit 1 - My server is up and running

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+  })
